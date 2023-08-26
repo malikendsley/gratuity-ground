f4ba56d6ffede69e06c16575c412e5609845b116
Malik Endsley <malik.s.endsley@gmail.com>
2023-08-25 20:48:20 -0400

    simple anim system with subscriber pattern, add walking, turning anims

--- Assets/Scripts/Player Mech/MechAnimationController.cs
+++ Assets/Scripts/Player Mech/MechAnimationController.cs
@@ -1,18 +1,64 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//TODO: Make this agnostic of the magic numbers inside the animation controller (Like 0.5 for walking, 1.0 for running for example)
 public class MechAnimationController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    [SerializeField]
+    private Animator anim;
+    [SerializeField]
+    private MechController mechController;
+    //TODO: Remove when this is tested
+    //Lets me see the value easily in the inspector
+
+#pragma warning disable IDE0052
+    [ReadOnly] public float speed;
+#pragma warning restore IDE0052
+
+    private void Awake()
     {
+        mechController.OnSpeedChangeAction += SetSpeed;
+        mechController.OnRotatingAction += SetRotating;
+    }
+
+    float NormalizeSpeed(float currentSpeed)
+    {
+        //TODO: Same anim for backward and forwards walking until more are addeed
+        currentSpeed = Math.Abs(currentSpeed);
+
+        float walkSpeed = mechController.GetMechLocomotionConfig().walkSpeed;
+        float runSpeed = mechController.GetMechLocomotionConfig().runSpeed;
+
+        if (currentSpeed <= walkSpeed)
+        {
+            return Mathf.Lerp(0f, 0.5f, currentSpeed / walkSpeed);
+        }
+        else
+        {
+            return Mathf.Lerp(0.5f, 1f, (currentSpeed - walkSpeed) / (runSpeed - walkSpeed));
+        }
+    }
+
+    void SetSpeed(float speed)
+    {
+        anim.SetFloat("Speed", NormalizeSpeed(speed));
+        // Inspection purposes only
+        this.speed = speed;
+    }
+
+    // Allows animator to react to turning (including in-place)
+    void SetRotating(MechController.RotationDirection dir)
+    {
+        // For now, just play the step animation at a slower speed, but allow it to be overriden by faster movements (turning while walking)
+        float newSpeed = (float)Math.Max(0.5, anim.GetFloat("Speed"));
+        anim.SetFloat("Speed", newSpeed);
+        speed = newSpeed;
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //TODO: Triggers for jump and bool for ground
+
 }
--- Assets/Scripts/Player Mech/MechController.cs
+++ Assets/Scripts/Player Mech/MechController.cs
@@ -22,8 +22,14 @@ public class MechController : MonoBehaviour
     private InputAction playerControls;
 
     // Actions for external users
-    public event Action<float> OnSpeedChange;
+    public event Action<float> OnSpeedChangeAction;
+    public event Action<RotationDirection> OnRotatingAction;
 
+    public enum RotationDirection
+    {
+        CW,
+        CCW
+    }
     private void OnEnable()
     {
         playerControls.Enable();
@@ -44,10 +50,10 @@ public class MechController : MonoBehaviour
         //TODO: Y needs to be flipped because for some reason the animations are backwards on the mech due to the way the bones were handled
         movementVector.y = -movementVector.y;
 
-        SetSpeed(movementVector);
-        //Linearly interpolate the character controller's current speed to target speed
+        UpdateTargetSpeed(movementVector);
         float acceleration = targetSpeed == 0f ? locomotionConfig.deceleration : locomotionConfig.acceleration;
         currentSpeed = Mathf.Lerp(currentSpeed, targetSpeed, acceleration * Time.deltaTime);
+        OnSpeedChangeAction?.Invoke(currentSpeed);
         Vector3 movement = currentSpeed * Time.deltaTime * transform.TransformDirection(Vector3.forward);
         Debug.Log("Calling Move with: " + movement);
         characterController.Move(movement);
@@ -55,7 +61,7 @@ public class MechController : MonoBehaviour
         Rotate(movementVector);
     }
 
-    public void SetSpeed(Vector2 movementVector)
+    public void UpdateTargetSpeed(Vector2 movementVector)
     {
         if (movementVector.y > 0)
         {
@@ -75,6 +81,11 @@ public class MechController : MonoBehaviour
     {
         float rotationAmount = movementVector.x * locomotionConfig.rotationSpeed * Time.deltaTime;
         transform.Rotate(0, rotationAmount, 0);
+        if (rotationAmount != 0)
+        {
+            //TODO: May need to flip
+            OnRotatingAction.Invoke(rotationAmount > 0 ? RotationDirection.CW : RotationDirection.CCW);
+        }
     }
 
     //Point the gun towards the target (TODO: should this take the target location, or a direction to look? Maybe override?)
@@ -83,6 +94,7 @@ public class MechController : MonoBehaviour
         // Implementation of the look function
     }
 
+    //TODO: Use actions to broadcast jumping and falling
 
     //TODO: Use these to drive the UI later
     //Expose the rotation and speed values
@@ -98,6 +110,11 @@ public class MechController : MonoBehaviour
         //Get the smallest angle in degrees from the original forward vector and the current head rotation
         Debug.LogError("Not implemented yet!");
         return 0;
+    }
+
+    public MechLocomotionConfig GetMechLocomotionConfig()
+    {
+        return locomotionConfig;
     }
 
 }
